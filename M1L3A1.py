4adbf8dcce8f28426c46c82b50d639352ec95f1a
NABEELA AKRAM <59659854+nabeela2597@users.noreply.github.com>
2025-12-12 20:42:54 +0530

    Update M1L3A1.py

--- M1L3A1.py
+++ M1L3A1.py
@@ -1,1 +1,10 @@
+#Odd Even
 
+number = int(input("Enter Number to check"))
+print("Number to be checked :", number)
+
+if number%2==0 :
+  print("This is an even number")
+
+else:
+  print("This is an odd number")
