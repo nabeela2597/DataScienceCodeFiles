a6146479c26c85480b8fe68f3831b0b4cc50787d
NABEELA AKRAM <59659854+nabeela2597@users.noreply.github.com>
2025-12-12 20:44:41 +0530

    Create M1L3A4.py

---
+++ M1L3A4.py
@@ -0,0 +1,13 @@
+#Date Time Operations
+import datetime  
+    
+# using now() to get current time  
+current_time = datetime.datetime.now()  
+    
+# Printing value of now.  
+print ("Time now at greenwich meridian is : ", end = "")   
+print (current_time)
+
+# print calendar of year 2021
+import calendar
+print("\n", calendar.calendar(2021))
