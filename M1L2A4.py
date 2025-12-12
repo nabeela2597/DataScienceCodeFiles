0168111456b7bc805b3e9c1822c8ed49f2b6b2eb
NABEELA AKRAM <59659854+nabeela2597@users.noreply.github.com>
2025-12-12 20:26:45 +0530

    Create M1L2A4.py

---
+++ M1L2A4.py
@@ -0,0 +1,12 @@
+#Number Swap
+x = input("Enter Value of x:")
+y = input("Enter Value of y:")
+
+# Swapping
+temp = x
+x = y
+y = temp
+
+# Displaying results after swapping
+print("value of x after swapping", x)
+print("value of y after swapping", y)
