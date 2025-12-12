bc76ea060bb10ccf387ecf74dae572b7ad031cd2
NABEELA AKRAM <59659854+nabeela2597@users.noreply.github.com>
2025-12-12 18:47:59 +0530

    Create M1L1A4.py

---
+++ M1L1A4.py
@@ -0,0 +1,12 @@
+'''Print a sentence and a number using Python Print Statement. Also, print all the keywords present in Python.'''
+
+#Print a Sentence
+print("Welcome to the world of programming!")
+#Print a Number
+print(4)
+#Next Line after print
+print("Hello Python \n")
+#Print all the keywords present in Python
+print("Keywords in Python are....")
+import keyword
+print(keyword.kwlist)
