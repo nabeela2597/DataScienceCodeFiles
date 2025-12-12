246e48ac088f89fb14b07b2df1c16253a148fcad
NABEELA AKRAM <59659854+nabeela2597@users.noreply.github.com>
2025-12-12 18:31:15 +0530

    Create M1L1.py

---
+++ M1L1.py
@@ -0,0 +1,10 @@
+'''Activity1 : Print Statements and Comments'''
+
+# Printing a greeting message
+print("Hello, world!")
+# Printing my name
+print("My name is Alex.")
+# Printing a fun fact about myself
+print("I love playing soccer.")
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
