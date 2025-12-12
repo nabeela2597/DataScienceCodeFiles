5ac7d1cbb7a57a6016dca32a4a206813502e5453
NABEELA AKRAM <59659854+nabeela2597@users.noreply.github.com>
2025-12-12 21:27:44 +0530

    Update M1L4A4.py

--- M1L4A4.py
+++ M1L4A4.py
@@ -6,9 +6,7 @@ if num > 1:
     if num % i == 0:
       print(f"{num} is not a prime number.")
       break
+    else:
+      print(f"{num} is a prime number.")
 else:
-print(f"{num} is a prime number.")
-
-else:
-
-print(f"{num} is not a prime number.")
+  print(f"{num} is not a prime number.")
